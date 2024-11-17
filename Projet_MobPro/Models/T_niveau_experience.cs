fe64afd84360992a76f43986f4557539ef8751ec
Neighley <justine.lncl@gmail.com>
2024-11-17 12:24:26 +0100

    [NivExpUser] : avant d'implémenter TempData pour les données entrées dans T_profil

--- Projet_MobPro/Models/T_niveau_experience.cs
+++ Projet_MobPro/Models/T_niveau_experience.cs
@@ -6,6 +6,7 @@
 //     Les modifications manuelles apportées à ce fichier sont remplacées si le code est régénéré.
 // </auto-generated>
 //------------------------------------------------------------------------------
+using System.ComponentModel.DataAnnotations;
 
 namespace Projet_MobPro.Models
 {
@@ -21,7 +22,9 @@ namespace Projet_MobPro.Models
         }
     
         public int id { get; set; }
+        [Display(Name = "Niveau d'expérience")]
         public string nom_niveau_experience { get; set; }
+        [Display(Name = "Domaine d'expérience")]
         public string domaine { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
